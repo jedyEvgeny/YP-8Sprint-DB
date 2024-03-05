0262468c1277df892193e63471823111d82e89b5
jedyEvgeny <97782789+jedyEvgeny@users.noreply.github.com>
2024-03-05 17:28:54 +1000

    Update main.go

--- main.go
+++ main.go
@@ -27,7 +27,6 @@ type ParcelService struct {
 }
 
 func main() {
-	// настройте подключение к БД
 	db, err := sql.Open("sqlite", "tracker.db")
 	if err != nil {
 		fmt.Println(err)
@@ -35,8 +34,8 @@ func main() {
 	}
 	defer db.Close()
 
-	store := NewParcelStore(db) // создайте объект ParcelStore функцией NewParcelStore
-	// В этот объект с единственным полем помещаем объект базы данных
+	store := NewParcelStore(db) // создаю объект ParcelStore функцией NewParcelStore
+	// В этот объект с единственным полем помещаю объект базы данных
 	service := NewParcelService(store)
 
 	// регистрация посылки
@@ -78,7 +77,6 @@ func main() {
 	}
 
 	// вывод посылок клиента
-	// предыдущая посылка не должна удалиться, т.к. её статус НЕ «зарегистрирована»
 	err = service.PrintClientParcels(client)
 	if err != nil {
 		fmt.Println(err)
@@ -100,7 +98,6 @@ func main() {
 	}
 
 	// вывод посылок клиента
-	// здесь не должно быть последней посылки, т.к. она должна была успешно удалиться
 	err = service.PrintClientParcels(client)
 	if err != nil {
 		fmt.Println(err)
